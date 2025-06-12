6515c47bb4f1a54802553f30e59d5609c735a893
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-06-12 14:07:19 +0300

    day 1 complete

--- 2015/day01/index.ts
+++ 2015/day01/index.ts
@@ -3,4 +3,47 @@ import fs from "fs";
 
 const input = fs.readFileSync("./input.txt", "utf-8").trim();
 
-console.log(input)
+// Part 1
+function calculateFloor() {
+  let floor = 0;
+  const floorUp = "(";
+  const floorDown = ")";
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === floorUp) {
+      floor += 1;
+    } else if (input[i] === floorDown) {
+      floor -= 1;
+    }
+  }
+
+  console.log("Floor: " + floor);
+};
+
+calculateFloor();
+
+// Part 2
+function basementPositionInInputData() {
+  let floor = 0;
+  const floorUp = "(";
+  const floorDown = ")";
+  let basementPosition = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === floorUp) {
+      floor += 1;
+    } else if (input[i] === floorDown) {
+      floor -= 1;
+    }
+
+    if (floor < 0) {
+      basementPosition = i + 1;
+
+      break;
+    }
+  }
+
+  console.log("Basement position: " + basementPosition);
+};
+
+basementPositionInInputData();
