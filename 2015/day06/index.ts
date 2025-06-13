fbc4cacb607988c670e059bd5f0932280580a421
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-06-13 22:50:07 +0300

    refactor for day 6 part 2, part 2 done

--- 2015/day06/index.ts
+++ 2015/day06/index.ts
@@ -22,15 +22,27 @@ function initializeGrid(rows: number, cols: number) {
   return grid;
 };
 
-function matrixOperation(grid: number[][], coords: { start: number[], end: number[] }, operation: "on" | "off" | "toggle") {
+function matrixOperation(grid: number[][], coords: { start: number[], end: number[] }, operation: "on" | "off" | "toggle", part: "1" | "2") {
   for (let i = coords.start[0]; i <= coords.end[0]; i++) {
     for (let j = coords.start[1]; j <= coords.end[1]; j++) {
       if (operation === "on") {
-        grid[i][j] = 1;
+        if (part === "1") {
+          grid[i][j] = 1;
+        } else {
+          grid[i][j] += 1;
+        }
       } else if (operation === "off") {
-        grid[i][j] = 0;
+        if (part === "1") {
+          grid[i][j] = 0;
+        } else {
+          grid[i][j] = grid[i][j] > 0 ? grid[i][j] - 1 : 0;
+        }
       } else {
-        grid[i][j] = !grid[i][j] ? 1 : 0;
+        if (part === "1") {
+          grid[i][j] = !grid[i][j] ? 1 : 0;
+        } else {
+          grid[i][j] += 2;
+        }
       }
     }
   }
@@ -38,7 +50,7 @@ function matrixOperation(grid: number[][], coords: { start: number[], end: numbe
   return grid;
 };
 
-function handleGridLights(lines: string[], grid: number[][]) {
+function handleGridLights(lines: string[], grid: number[][], part: "1" | "2") {
   for (const line of lines) {
     const splittedLine = line.split(" ");
 
@@ -48,34 +60,42 @@ function handleGridLights(lines: string[], grid: number[][]) {
         end: splittedLine[3].split(",").map(Number),
       };
 
-      grid = matrixOperation(grid, coords, "toggle");
+      grid = matrixOperation(grid, coords, "toggle", part);
     } else if (splittedLine[1] === "on") {
       const coords = {
         start: splittedLine[2].split(",").map(Number),
         end: splittedLine[4].split(",").map(Number),
       };
 
-      grid = matrixOperation(grid, coords, "on");
+      grid = matrixOperation(grid, coords, "on", part);
     } else {
       const coords = {
         start: splittedLine[2].split(",").map(Number),
         end: splittedLine[4].split(",").map(Number),
       };
 
-      grid = matrixOperation(grid, coords, "off");
+      grid = matrixOperation(grid, coords, "off", part);
     }
   }
 
   return grid;
 };
 
-function calculateLightsOn(grid: number[][]) {
+function calculateLightsOn(grid: number[][], part: "1" | "2") {
   let lightsOn = 0;
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j]) {
-        lightsOn++;
+  if (part === "1") {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        if (grid[i][j]) {
+          lightsOn++;
+        }
+      }
+    }
+  } else {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        lightsOn += grid[i][j];
       }
     }
   }
@@ -83,8 +103,12 @@ function calculateLightsOn(grid: number[][]) {
   console.log("Number of lights on: " + lightsOn);
 }
 
-const grid = handleGridLights(lines, initializeGrid(rows, cols));
+const grid = handleGridLights(lines, initializeGrid(rows, cols), "1");
 
-calculateLightsOn(grid);
+calculateLightsOn(grid, "1");
 
 // Part 2
+
+const grid2 = handleGridLights(lines, initializeGrid(rows, cols), "2");
+
+calculateLightsOn(grid2, "2");
