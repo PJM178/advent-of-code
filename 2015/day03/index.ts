ee568a2cc1471392856b2d05766aa8e300a3767b
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-06-13 11:42:21 +0300

    day 3 part 2

--- 2015/day03/index.ts
+++ 2015/day03/index.ts
@@ -26,9 +26,44 @@ function calculateHousesServed() {
     visitedCoordinates.add(coordinates.join(","));
   }
 
-  console.log("Unique houses visited: " +  visitedCoordinates.size);
+  console.log("Unique houses visited: " + visitedCoordinates.size);
 }
 
 calculateHousesServed();
 
 // Part 2
+function calculateHousesServedHelper() {
+  const visitedCoordinates = new Set();
+  let santaCoordinates = [0, 0];
+  let roboCoordinates = [0, 0];
+  visitedCoordinates.add(santaCoordinates.join(","));
+
+  const movement = {
+    ">": [1, 0],
+    "v": [0, -1],
+    "<": [-1, 0],
+    "^": [0, 1],
+  };
+
+  for (let i = 0; i < input.length; i++) {
+    const coord = movement[input[i] as keyof typeof movement];
+
+    if (i % 2 === 0) {
+      for (let i = 0; i < santaCoordinates.length; i++) {
+        santaCoordinates[i] += coord[i];
+      }
+
+      visitedCoordinates.add(santaCoordinates.join(","));
+    } else {
+      for (let i = 0; i < santaCoordinates.length; i++) {
+        roboCoordinates[i] += coord[i];
+      }
+
+      visitedCoordinates.add(roboCoordinates.join(","));
+    }
+  }
+
+  console.log("Unique houses visited by Santa and the helper: " + visitedCoordinates.size);
+}
+
+calculateHousesServedHelper();
