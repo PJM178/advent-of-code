3fcfd4aca6b67396a4f37bfd8567ad3944e7c1cf
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-06-13 13:12:43 +0300

    day 5 part 1

---
+++ 2015/day05/index.ts
@@ -0,0 +1,65 @@
+// URL: https://adventofcode.com/2015/day/5
+import fs from "fs";
+
+const input = fs.readFileSync("./input.txt", "utf-8").trim();
+const lines = input.split("\n");
+const bannedStrings = new Map();
+
+
+// Part 1
+function checkTwiceInRowCharacters(str: string): boolean {
+  let previousC = "";
+
+  for (const c of str) {
+    if (previousC === c) {
+      return true;
+    }
+
+    previousC = c;
+  }
+
+  return false;
+}
+
+function checkIfContainsVowels(str: string): boolean {
+  let vowelNumber = 0;
+  const vowels = "aeiou";
+
+  for (const c of str) {
+    if (vowels.includes(c)) {
+      vowelNumber += 1;
+
+      if (vowelNumber === 3) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function checkIfNotContainString(str: string): boolean {
+  const bannedStrings = ["ab", "cd", "pq", "xy"];
+
+  for (const string of bannedStrings) {
+    if (str.includes(string)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function calculateNiceStrings(strings: string[]) {
+  let niceStrings = 0;
+
+  for (const string of strings) {
+    if (checkIfNotContainString(string) && checkTwiceInRowCharacters(string) && checkIfContainsVowels(string) ) {
+      niceStrings += 1;
+    }
+  }
+
+  console.log("Number of nice strings: " + niceStrings);
+}
+
+calculateNiceStrings(lines);
