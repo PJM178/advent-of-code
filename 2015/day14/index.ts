0f1352b0246e7f2238e68a4cabbb46ffa333126d
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-06-25 16:14:07 +0300

    day 14 part 2

--- 2015/day14/index.ts
+++ 2015/day14/index.ts
@@ -11,6 +11,8 @@ interface DataObject {
   rest: number;
   traveled: number;
   isResting: boolean;
+  cycle: number;
+  points: number;
 }
 
 function formatData(data: string[]) {
@@ -23,6 +25,8 @@ function formatData(data: string[]) {
       rest: 0,
       traveled: 0,
       isResting: false,
+      cycle: 0,
+      points: 0,
     };
     const splittedLine = data[i].split(" ");
     parameterObject["speed"] = Number(splittedLine[3]);
@@ -69,3 +73,50 @@ function flyingDistance(data: Record<string, DataObject>, flightDuration: number
 const winningDistance = flyingDistance(data, 2503);
 
 console.log("The distance the winning reindeer has travelled: " + winningDistance[0] + ", " + winningDistance[1].traveled);
+
+// Part 2
+function allocatePoints(data: Record<string, DataObject>, flightDuration: number): [string, DataObject] {
+  const keys = Object.keys(data);
+
+  for (let i = 0; i < flightDuration; i++) {
+    let maxValue = 0;
+
+    for (const key of keys) {
+      const obj = data[key];
+      obj.cycle++;
+
+      if (obj.cycle <= obj.duration) {
+        obj.traveled += obj.speed;
+      } else {
+        if (obj.cycle === (obj.rest + obj.duration)) {
+          obj.cycle = 0;
+        }
+      }
+
+      if (obj.traveled > maxValue) {
+        maxValue = obj.traveled;
+      }
+    }
+
+    for (const key of keys) {
+      const obj = data[key];
+
+      if (obj.traveled === maxValue) {
+        obj.points++;
+      }
+    }
+  }
+
+  const entries = Object.entries(data);
+  const [winner, object] = entries.reduce((maxEntry, currentEntry) => {
+    return currentEntry[1].points > maxEntry[1].points ? currentEntry : maxEntry;
+  });
+
+  return [winner, object];
+}
+
+const dataPart2 = formatData(lines);
+
+const winningPoints = allocatePoints(dataPart2, 2503);
+
+console.log("The points the winning reindeer has: " + winningPoints[0] + ", " + winningPoints[1].points);
